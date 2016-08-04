798514fd0a049827ddae4fae5378e4518df49dd2
mithun <mithunf@qburst.com>
2016-08-04 12:46:14 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,10 @@
+var json = {
+						    "imageItems": [
+						    				 {"images": "img/176-Need-For-Speed.png"},
+						    				 {"images": "img/assassins-creed-escape-room-paris.jpg"},
+						     				 {"images": "img/Battlefield-4-Fishing-in-Baku-screen-640x420.jpg"},
+						     				 {"images": "img/far-cry-3.jpg"},
+						     				 {"images": "img/jaquette-call-of-duty-black-ops-ii-vengeance-pc-cover-avant-g-1371654967.jpg"},
+						     				 {"images": "img/medium_cars-amazing-fullhd-background-aston-martin-picture-free-high.jpg"}
+						    				]
+						};
